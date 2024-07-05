9bbf577b97bee18e418365c3fa1d85815c998620
Alexander K <alexkuz@gmail.com>
2024-07-05 20:09:28 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://alexkuz.github.io',
+  base: 'module-replacements-docs',
+});
